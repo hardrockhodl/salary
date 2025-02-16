a7a5d024877527c507abf46c589f52aa59fadae0
hardrockhodl <105864199+hardrockhodl@users.noreply.github.com>
2025-02-16 18:13:38 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -100,14 +100,13 @@ function App() {
 
   return (
     <>
-      <img src="https://raw.githubusercontent.com/hardrockhodl/salary/b5dd8313e15eea5473e4c805db21a9f6a9336047/src/images/conscia.svg" alt="Conscia" className="mt-2 mr-4 ml-auto block"/>
-        <h1 className="text-xl font-bold text-[#238DD8] mt-0 mr-4 mb-4 gap-4 text-end">
-          {/* <DollarSign className="w-8 h-8 text-gray-900" /> */}
+      <img src="src/images/conscia.svg" alt="Conscia" className="mt-2 mr-4 ml-auto block w-24"/>
+        <h1 className="text-base font-bold text-[#238DD8] mt-0 mr-4 mb-2 gap-2 text-end">
           Salary Calculator
         </h1>
-      <div className="pl-4 pr-4 grid gap-4 md:grid-cols-3">
-        <div className="p-6 bg-[#238DD8] rounded-xl shadow-sm">
-          <h2 className="text-xl font-semibold text-[#F3F4F6] mb-4 flex items-center gap-2">
+      <div className="pl-4 pr-4 grid gap-2 md:grid-cols-3">
+        <div className="p-4 bg-[#238DD8] rounded-xl shadow-sm">
+          <h2 className="text-base font-semibold text-[#F3F4F6] mb-4 flex items-center gap-2">
             <Table className="w-5 h-5 text-[#F3F4F6]" />
             Välj Kommun
           </h2>
@@ -117,7 +116,7 @@ function App() {
               setSelectedKommun(e.target.value);
               setSelectedFörsamling("");
             }}
-            className="mt-2 block w-full rounded-md bg-gray-100 border-orange-500 shadow-sm outline-none focus-within:outline-3 focus-within:outline-[#FF9655]"
+            className="mt-2 text-sm block w-full rounded-md bg-gray-100 border-orange-500 shadow-sm outline-none focus-within:outline-3 focus-within:outline-[#FF9655]"
           >
             <option value="">-- Välj kommun --</option>
             {kommuner.map((kommun) => (
@@ -132,20 +131,20 @@ function App() {
                 type="checkbox"
                 checked={isChurchMember}
                 onChange={(e) => setIsChurchMember(e.target.checked)}
-                className="form-checkbox mt-2 h-4 w-4 text-emerald-600"
+                className="form-checkbox mt-2 h-3 w-3 text-emerald-600"
               />
-              <span className="ml-2 pt-2 text-gray-50">Medlem i Svenska kyrkan</span>
+              <span className="ml-2 pt-2 text-sm text-gray-50">Medlem i Svenska kyrkan</span>
             </label>
           </div>
           {isChurchMember && selectedKommun && (
-            <div className="mb-2">
-              <label className="block text-m font-medium text-gray-50">
+            <div className="mt-2">
+              <label className="block text-sm font-medium text-gray-50">
                 Välj Församling:
               </label>
               <select
                 value={selectedFörsamling}
                 onChange={(e) => setSelectedFörsamling(e.target.value)}
-                className="mt-1 block w-full rounded-md bg-gray-100 shadow-sm focus:ring-blue-200"
+                className="mt-2 text-sm block w-full rounded-md bg-gray-100 shadow-sm focus:ring-blue-200"
               >
                 <option value="">-- Välj församling --</option>
                 {församlingar.map((församling) => (
@@ -157,8 +156,8 @@ function App() {
             </div>
           )}
         </div>
-        <div className="p-6 bg-[#238DD8] rounded-xl shadow-sm">
-          <h2 className="text-xl font-semibold text-[#F3F4F6] mb-4 flex items-center gap-4">
+        <div className="p-4 bg-[#238DD8] rounded-xl shadow-sm">
+          <h2 className="text-base font-semibold text-[#F3F4F6] mb-4 flex items-center gap-4">
             <Wallet className="w-5 h-5 text-[#F3F4F6]" />
             Grundlön
           </h2>
@@ -166,11 +165,11 @@ function App() {
             type="number"
             value={baseSalary}
             onChange={(e) => setBaseSalary(Number(e.target.value))}
-            className="mt-2 block w-full rounded-md border-orange-500 shadow-sm outline-none focus-within:outline-3 focus-within:outline-[#FF9655] font-mono"
+            className="mt-2 text-sm block w-full rounded-md border-orange-500 shadow-sm outline-none focus-within:outline-3 focus-within:outline-[#FF9655] font-mono"
           />
         </div>
-        <div className="p-6 bg-[#238DD8] rounded-xl shadow-sm">
-          <h2 className="text-xl font-semibold text-[#F3F4F6] mb-4 flex items-center gap-2">
+        <div className="p-4 bg-[#238DD8] rounded-xl shadow-sm">
+          <h2 className="text-base font-semibold text-[#F3F4F6] mb-4 flex items-center gap-2">
             <Clock className="w-5 h-5 text-[#F3F4F6]" />
             Debiterbara timmar
           </h2>
@@ -181,11 +180,11 @@ function App() {
               setIncome({ ...income, billableHours: Number(e.target.value) })
             }
             placeholder="Ange timmar..."
-            className="mt-2 block w-full rounded-md shadow-sm outline-none focus-within:outline-3 focus-within:outline-[#FF9655] font-mono"
+            className="mt-2 text-sm  block w-full rounded-md shadow-sm outline-none focus-within:outline-3 focus-within:outline-[#FF9655] font-mono"
           />
         </div>
-        <div className="p-6 bg-[#238DD8] rounded-xl shadow-sm">
-          <h2 className="text-xl font-semibold text-[#F3F4F6] mb-4 flex items-center gap-2">
+        <div className="p-4 bg-[#238DD8] rounded-xl shadow-sm">
+          <h2 className="text-base font-semibold text-[#F3F4F6] mb-4 flex items-center gap-2">
             <Phone className="w-5 h-5 text-[#F3F4F6]" />
             Beredskap
           </h2>
@@ -196,7 +195,7 @@ function App() {
             onChange={(e) =>
               setIncome({ ...income, onCallDays: Number(e.target.value) })
             }
-            className="mt-2 block w-full rounded-md border-orange-500 shadow-sm outline-none focus-within:outline-3 focus-within:outline-[#FF9655] font-mono"
+            className="mt-2 text-sm block w-full rounded-md border-orange-500 shadow-sm outline-none focus-within:outline-3 focus-within:outline-[#FF9655] font-mono"
           />
           <input
             type="number"
@@ -208,8 +207,8 @@ function App() {
             className="mt-2 block w-full rounded-md border-orange-500 shadow-sm outline-none focus-within:outline-3 focus-within:outline-[#FF9655] font-mono"
           />
         </div>
-        <div className="p-6 bg-[#238DD8] rounded-xl shadow-sm">
-          <h2 className="text-xl font-semibold text-[#F3F4F6] mb-4 flex items-center gap-2">
+        <div className="p-4 bg-[#238DD8] rounded-xl shadow-sm">
+          <h2 className="text-base font-semibold text-[#F3F4F6] mb-4 flex items-center gap-2">
             <Calendar className="w-5 h-5 text-[#F3F4F6]" />
             On Call Service
           </h2>
@@ -220,7 +219,7 @@ function App() {
             onChange={(e) =>
               setIncome({ ...income, appointments: Number(e.target.value) })
             }
-            className="mt-2 block w-full rounded-md border-orange-500 shadow-sm outline-none focus-within:outline-3 focus-within:outline-[#FF9655] font-mono"
+            className="mt-2 text-sm block w-full rounded-md border-orange-500 shadow-sm outline-none focus-within:outline-3 focus-within:outline-[#FF9655] font-mono"
           />
           <input
             type="number"
@@ -229,11 +228,11 @@ function App() {
             onChange={(e) =>
               setIncome({ ...income, appointmentRate: Number(e.target.value) })
             }
-            className="mt-2 block w-full rounded-md border-orange-500 shadow-sm outline-none focus-within:outline-3 focus-within:outline-[#FF9655] font-mono"
+            className="mt-2 text-sm block w-full rounded-md border-orange-500 shadow-sm outline-none focus-within:outline-3 focus-within:outline-[#FF9655] font-mono"
           />
         </div>
-        <div className="p-6 bg-[#238DD8] rounded-xl shadow-sm">
-          <h2 className="text-xl font-semibold text-[#F3F4F6] mb-4 flex items-center gap-2">
+        <div className="p-4 bg-[#238DD8] rounded-xl shadow-sm">
+          <h2 className="text-base font-semibold text-[#F3F4F6] mb-2 flex items-center gap-2">
             <HandCoins className="w-5 h-5 text-[#F3F4F6]" />
             Utlägg
           </h2>
@@ -242,32 +241,32 @@ function App() {
             value={expenseAmount}
             onChange={(e) => setExpenseAmount(Number(e.target.value))}
             placeholder="Ange utlägg..."
-            className="mt-2 block w-full rounded-md shadow-sm outline-none focus-within:outline-3 focus-within:outline-[#FF9655] font-mono"
+            className="mt-2 text-sm block w-full rounded-md shadow-sm outline-none focus-within:outline-3 focus-within:outline-[#FF9655] font-mono"
           />
         </div>
       </div>
 
 
 
-      <div className="mt-8 ml-4 mr-4 grid grid-cols-1 md:grid-cols-3 gap-4">
+      <div className="mt-2 ml-4 mr-4 grid grid-cols-1 md:grid-cols-3 gap-2">
       {/* Pension Section */}
-      <div className="bg-[#033258] p-6 rounded-lg shadow-lg flex flex-col h-[350px]">
+      <div className="bg-[#033258] p-4 rounded-lg shadow-lg flex flex-col h-[250px]">
         {/* Pension Info */}
-        <div className="flex-grow space-y-4">
-          <h2 className="text-2xl font-bold text-gray-50 mb-4">Pension</h2>
-          <div className="flex justify-between">
+        <div className="flex-grow space-y-1">
+          <h2 className="text-lg font-bold text-gray-50 mb-4">Pension</h2>
+          <div className="flex text-sm justify-between">
             <span className="text-gray-200">Tjänstepension (4.5%):</span>
             <span className="font-mono text-gray-200">{formatCurrency(
               Math.min(baseSalary, 50375) * 0.045
             )}</span>
           </div>
-          <div className="flex justify-between">
+          <div className="flex text-sm justify-between">
             <span className="text-gray-200">Tjänstepension (30%):</span>
             <span className="font-mono text-gray-200">{formatCurrency(
               baseSalary > 50375 ? (baseSalary - 50375) * 0.30 : 0
             )}</span>
           </div>
-          <div className="flex justify-between text-xl font-semibold">
+          <div className="flex justify-between text-base font-semibold">
             <span className="text-gray-50">Totalt:</span>
             <span className="font-mono text-emerald-400">{formatCurrency(
               Math.min(baseSalary, 50375) * 0.045 + (baseSalary > 50375 ? (baseSalary - 50375) * 0.30 : 0)
@@ -276,15 +275,15 @@ function App() {
 
           {/* Provision Pension Section */}
           {includeCommissionInPension && (
-            <div className="flex justify-between text-m">
+            <div className="flex text-sm justify-between">
               <span className="text-gray-200">inkl Provision:</span>
-              <span className="font-mono text-xl font-semibold text-emerald-400">{formatCurrency(totalPension)}</span>
+              <span className="font-mono text-base font-semibold text-emerald-400">{formatCurrency(totalPension)}</span>
             </div>
           )}
         </div>
 
         {/* Kryssruta längst ned */}
-        <div className="mt-4">
+        <div className="mt-0">
           <label className="inline-flex items-center">
             <input
               type="checkbox"
@@ -292,35 +291,35 @@ function App() {
               onChange={(e) => setIncludeCommissionInPension(e.target.checked)}
               className="form-checkbox h-4 w-4 text-emerald-600"
             />
-            <span className="ml-2 text-gray-600">Räkna provision som pensionsgrundande</span>
+            <span className="ml-2 text-sm  text-gray-600">Räkna provision som pensionsgrundande</span>
           </label>
         </div>
       </div>
 
       {/* Tax Information */}
-      <div className="bg-[#033258] p-6 rounded-lg shadow-lg">
-        <h2 className="text-2xl font-bold text-gray-50 mb-6">Skatteinformation</h2>
+      <div className="bg-[#033258] p-4 rounded-lg shadow-lg">
+        <h2 className="text-lg font-bold text-gray-50 mb-2">Skatteinformation</h2>
         {currentTaxRate ? (
-          <div className="space-y-4">
-            <div className="flex justify-between">
+          <div className="space-y-1">
+            <div className="flex text-sm justify-between">
               <span className="text-gray-200">Kommunal skatt:</span>
               <span className="font-mono text-gray-200">{currentTaxRate["Kommunal-skatt"]}%</span>
             </div>
-            <div className="flex justify-between">
+            <div className="flex text-sm justify-between">
               <span className="text-gray-200">Landstingsskatt:</span>
               <span className="font-mono text-gray-200">{currentTaxRate["Landstings-skatt"]}%</span>
             </div>
-            <div className="flex justify-between">
+            <div className="flex text-sm justify-between">
               <span className="text-gray-200">Begravningsavgift:</span>
               <span className="font-mono text-gray-200">{currentTaxRate["Begravnings-avgift"]}%</span>
             </div>
             {isChurchMember && (
-              <div className="flex justify-between">
+              <div className="flex text-sm justify-between">
                 <span className="text-gray-200">Kyrkoavgift:</span>
                 <span className="font-mono text-gray-200">{currentTaxRate["Kyrkoavgift"]}%</span>
               </div>
             )}
-            <div className="flex justify-between text-xl font-bold">
+            <div className="flex justify-between text-base font-bold">
               <span className="text-gray-200">Skatt:</span>
               <span className="font-mono text-red-500">- 
                 {formatCurrency(
@@ -336,40 +335,40 @@ function App() {
         )}
       </div>
       {/* Summering Section */}
-      <div className="bg-[#033258] p-6 rounded-lg shadow-lg">
-        <h2 className="text-2xl font-bold text-gray-50 mb-6">Summering</h2>
-        <div className="space-y-4">
-          <div className="flex justify-between">
+      <div className="bg-[#033258] p-4 rounded-lg shadow-lg">
+        <h2 className="text-lg font-bold text-gray-50 mb-2">Summering</h2>
+        <div className="space-y-1">
+          <div className="flex text-sm justify-between">
             <span className="text-gray-200">Grundlön:</span>
             <span className="font-mono text-gray-200">{formatCurrency(baseSalary)}</span>
           </div>
-          <div className="flex justify-between">
+          <div className="flex text-sm justify-between">
             <span className="text-gray-200">Beredskap:</span>
             <span className="font-mono text-gray-200">{formatCurrency(income.onCallDays * income.onCallRate)}</span>
           </div>
-          <div className="flex justify-between">
+          <div className="flex text-sm justify-between">
             <span className="text-gray-200">On Call Service:</span>
             <span className="font-mono text-gray-200">{formatCurrency(income.appointments * income.appointmentRate)}</span>
           </div>
-          <div className="flex justify-between">
+          <div className="flex text-sm justify-between">
             <span className="text-gray-200">Provision:</span>
             <span className="font-mono text-gray-200">{formatCurrency(
               income.billableHours > 100 ? (income.billableHours - 100) * calculateCommissionRate(income.billableHours) : 0
             )}</span>
           </div>
-          <div className="flex justify-between text-xl font-semibold">
+          <div className="flex justify-between text-base font-semibold">
             <span className="text-gray-200">Bruttolön:</span>
             <span className="font-mono text-gray-200">{formatCurrency(grossSalary)}</span>
           </div>
-          <div className="flex justify-between">
+          <div className="flex text-sm justify-between">
             <span className="text-gray-200">Skatt:</span>
             <span className="font-mono text-red-500">-{formatCurrency(taxAmount)}</span>
           </div>
-          <div className="flex justify-between">
+          <div className="flex text-sm justify-between">
             <span className="text-gray-200">Utlägg:</span>
             <span className="font-mono text-gray-200">{formatCurrency(expenseAmount)}</span>
           </div>
-          <div className="flex justify-between text-xl font-bold">
+          <div className="flex justify-between text-base font-bold">
             <span className="text-emerald-400">Nettolön:</span>
             <span className="font-mono text-emerald-400">{formatCurrency(netSalary)}</span>
           </div>
